1c637338f81a2c05002a2796dad0134adc58e395
Matheus de Mello Queiroz <m.dimello@hotmail.com>
2019-05-01 21:34:00 +0530

    Hello, World!

---
+++ app/src/main/java/me/mqueiroz/github/MainActivity.kt
@@ -0,0 +1,14 @@
+package me.mqueiroz.github
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
