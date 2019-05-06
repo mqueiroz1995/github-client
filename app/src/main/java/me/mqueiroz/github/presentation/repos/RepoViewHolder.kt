3123817904361d008c7b44291cdee7879a8dd736
Matheus de Mello Queiroz <m.dimello@hotmail.com>
2019-05-06 23:01:14 +0530

    Adds progress bar to image loading

--- app/src/main/java/me/mqueiroz/github/presentation/repos/RepoViewHolder.kt
+++ app/src/main/java/me/mqueiroz/github/presentation/repos/RepoViewHolder.kt
@@ -1,13 +1,16 @@
 package me.mqueiroz.github.presentation.repos
 
 import android.view.View
+import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.list_item_repo.view.*
+import me.mqueiroz.github.R
 import me.mqueiroz.github.model.data.Repo
 import me.mqueiroz.github.presentation.base.BaseViewHolder
+import java.lang.Exception
 
 class RepoViewHolder(
-    itemView: View
+        itemView: View
 ) : BaseViewHolder<Repo>(itemView) {
 
     private val name = itemView.repo_name
@@ -16,6 +19,7 @@ class RepoViewHolder(
     private val stars = itemView.repo_stars
     private val image = itemView.repo_image
     private val ownerUsername = itemView.repo_owner_username
+    private val progressBar = itemView.progressBar
 
     override fun bind(obj: Repo) {
         name.text = obj.name
@@ -24,6 +28,18 @@ class RepoViewHolder(
         stars.text = obj.stargazersCount.toString()
         ownerUsername.text = obj.owner.login
 
-        Picasso.get().load(obj.owner.avatarUrl).into(image)
+        progressBar.visibility = View.VISIBLE
+        Picasso.get()
+                .load(obj.owner.avatarUrl)
+                .error(R.drawable.ic_error_outline)
+                .into(image, object : Callback {
+                    override fun onSuccess() {
+                        progressBar.visibility = View.GONE
+                    }
+
+                    override fun onError(e: Exception?) {
+                        progressBar.visibility = View.GONE
+                    }
+                })
     }
 }
